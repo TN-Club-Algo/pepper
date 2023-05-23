1d445e9cf2960b54fbd108cc5b85ad2de3b1efe9
DUCHESNE Alexandre <alexandre.duchesne@telecomnancy.eu>
2023-05-23 08:30:55 +0200

    pepper main loop

--- pepper/main.go
+++ pepper/main.go
@@ -1,6 +1,17 @@
 package main
 
+import "fmt"
+
 func main() {
 	//Connect("127.0.0.1", "")
-	StartVM("/root/test-vm")
+	var cmd string
+	for {
+		_, err := fmt.Scanln(&cmd)
+		if err != nil {
+			fmt.Println(cmd)
+			if cmd == "startvm" {
+				StartVM("/root/test-vm")
+			}
+		}
+	}
 }
